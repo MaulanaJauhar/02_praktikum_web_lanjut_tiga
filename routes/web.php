ddca763ce85573f6821a8c32b9f7f71061f838a5
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-01 16:57:54 +0700

    Feature Routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// No 1
 Route::get('/', function () {
-    return view('welcome');
+    return 'Halaman Home';
 });
+
+// No 2
+Route::prefix('category')->group(function () {
+    Route::get('/marbel-edu-games', function () {
+        return "Halaman Marbel Edu Games";
+    });
+    Route::get('/marbel-and-friends-kids-games', function () {
+        return "Halaman Marbel and Friends Kids Games";
+    });
+    Route::get('/riri-story-books', function () {
+        return "Halaman Riri Story Books";
+    });
+    Route::get('/kolak-kids-songs', function () {
+        return "Halaman Kolak Kids Songs";
+    });
+});
+
+//No 3
+Route::get('/news/{title?}', function ($title = null) {
+    return "News ".$title;
+});
+
+//No 4
+Route::prefix('program')->group(function () {
+    Route::get('/karir', function () {
+        return "Halaman Karir Polinema";
+    });
+    Route::get('/magang', function () {
+        return "Halaman Magang Polinema";
+    });
+    Route::get('/kunjungan-industri', function () {
+        return "Halaman Kunjungan Industri Polinema";
+    });
+}); 
+
+//No 5
+Route::get('/about-us', function () {
+    return 'Halaman About Our Company';
+});
+
+//No 6
+Route::resource('contact-us', ContactController::class)->only(['index', 'store']);
